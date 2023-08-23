de71e400d3304643fd30a2f5aaa6345ac65377e6
Beees835 <hlin0051@student.monash.edu>
2023-08-24 01:56:44 +1000

    Code involved for loop remake everything

--- src/main.ts
+++ src/main.ts
@@ -19,12 +19,6 @@ import { map, filter, scan } from "rxjs/operators";
 
 /** Constants */
 
-
-// Constants as required 10 x 20 
-const BOARD_WIDTH = 10;
-const BOARD_HEIGHT = 21;
-
-
 const Viewport = {
   CANVAS_WIDTH: 200,
   CANVAS_HEIGHT: 400,
@@ -43,46 +37,6 @@ const Block = {
   HEIGHT: Viewport.CANVAS_HEIGHT / Constants.GRID_HEIGHT,
 };
 
-// All shapes of Tetrominos 
-const I_TETROMINO = [
-  [1, 1, 1, 1] 
-]; 
-
-const O_TETROMINO = [
-  [1, 1],
-  [1, 1]
-];
-
-const T_TETROMINO = [ 
-  [1, 1, 1], 
-  [0, 1, 0]
-]
-
-const S_TETROMINO = [
-  [0, 1, 1],
-  [1, 1, 0]
-];
-
-const Z_TETROMINO = [
-  [1, 1, 0],
-  [0, 1, 1]
-];
-
-const J_TETROMINO = [
-  [1, 0, 0],
-  [1, 1, 1]
-];
-
-const L_TETROMINO = [
-  [0, 0, 1],
-  [1, 1, 1]
-];
-
-const TETROMINOES = [I_TETROMINO, O_TETROMINO, T_TETROMINO, S_TETROMINO, Z_TETROMINO, J_TETROMINO, L_TETROMINO];
-
-const randomTetromino = TETROMINOES[Math.floor(Math.random() * TETROMINOES.length)];
-
-
 /** User input */
 
 type Key = "KeyS" | "KeyA" | "KeyD";
@@ -92,28 +46,13 @@ type Event = "keydown" | "keyup" | "keypress";
 /** Utility functions */
 
 /** State processing */
-type Tetromino = {
-  shape: number[][];
-  posX: number;
-  posY: number;
-};
 
 type State = Readonly<{
   gameEnd: boolean;
-  board: number[][];
-  currentTetromino: Tetromino;
 }>;
 
 const initialState: State = {
   gameEnd: false,
-  board: Array(BOARD_HEIGHT).fill(Array(BOARD_WIDTH).fill(0)),
-
-  // This is how we let the Tetrominos appear on the screen 
-  currentTetromino: {
-    shape: TETROMINOES[Math.floor(Math.random() * TETROMINOES.length)],
-    posX: Math.floor(BOARD_WIDTH / 2) - 1, // Center the tetromino
-    posY: 0,
-  }
 } as const;
 
 /**
@@ -122,42 +61,7 @@ const initialState: State = {
  * @param s Current state
  * @returns Updated state
  */
-const tick = (s: State) => {
-  let newY = s.currentTetromino.posY + 1;
-  if (canMoveTo(s.currentTetromino.shape, s.board, s.currentTetromino.posX, newY)) {
-      return {
-          ...s,
-          currentTetromino: {
-              ...s.currentTetromino,
-              posY: newY
-          }
-      };
-  } else {
-      // The tetromino has landed. You can merge it with the board and spawn a new tetromino here.
-      return s;
-  }
-};
-
-function canMoveTo(tetromino: number[][], board: number[][], posX: number, posY: number): boolean {
-  for (let y = 0; y < tetromino.length; y++) {
-      for (let x = 0; x < tetromino[y].length; x++) {
-          if (tetromino[y][x] === 1) {
-              if (
-                  // Check if out of board boundaries
-                  posY + y > BOARD_HEIGHT - 1||
-                  posX + x >= BOARD_WIDTH ||
-                  posX + x < 0 ||
-                  // Check if overlaps with another block
-                  board[posY + y][posX + x] !== 0
-              ) {
-                  return false;
-              }
-          }
-      }
-  }
-  return true;
-}
-
+const tick = (s: State) => s;
 
 /** Rendering (side effects) */
 
@@ -235,29 +139,6 @@ export function main() {
 
   /** Observables */
 
-  // Helper functions
-
-  function canMove(shape: number[][], board: number[][], posX: number, posY: number): boolean {
-    for (let y = 0; y < shape.length; y++) {
-        for (let x = 0; x < shape[y].length; x++) {
-            if (
-                shape[y][x] && // Part of the Tetromino
-                (
-                    // Check if out of board bounds
-                    y + posY >= BOARD_HEIGHT ||
-                    x + posX >= BOARD_WIDTH ||
-                    x + posX < 0 ||
-                    // or collides with another block
-                    board[y + posY][x + posX] !== 0
-                )
-            ) {
-                return false;
-            }
-        }
-    }
-    return true;
-}
-  
   /** Determines the rate of time steps */
   const tick$ = interval(Constants.TICK_RATE_MS);
 
@@ -269,11 +150,6 @@ export function main() {
    * @param s Current state
    */
   const render = (s: State) => {
-   
-    if (s.currentTetromino) {
-      renderTetromino(s.currentTetromino.shape, s.currentTetromino.posX, s.currentTetromino.posY, "orange", svg);
-    }
-
     // Add blocks to the main grid canvas
     const cube = createSvgElement(svg.namespaceURI, "rect", {
       height: `${Block.HEIGHT}`,
@@ -309,87 +185,19 @@ export function main() {
       style: "fill: green",
     });
     preview.appendChild(cubePreview);
-  } 
+  };
 
-  function renderBlock(x: number, y: number, color: string, svg: SVGGraphicsElement) {
-    const rect = createSvgElement(svg.namespaceURI, "rect", {
-      x: `${x * Block.WIDTH}`,
-      y: `${y * Block.HEIGHT}`,
-      width: `${Block.WIDTH}`,
-      height: `${Block.HEIGHT}`,
-      style: `fill:${color}`
-    });
-    svg.appendChild(rect);
-  }
-  
-
-  function renderTetromino(tetromino: number[][], posX: number, posY: number, color: string, svg: SVGGraphicsElement) {
-    const tetrominoHeight = tetromino.length;
-    const adjustedPosY = posY - (tetrominoHeight - 1);
-    for (let y = 0; y < tetrominoHeight; y++) {
-        for (let x = 0; x < tetromino[y].length; x++) {
-            if (tetromino[y][x] === 1) {
-                renderBlock(posX + x, adjustedPosY + y, color, svg);
-            }
-        }
-    }
-}
-
-  
-
-  // return true of false if there is any blocks reach the 21 space cloumn end the game (true) else false;    
-  // fnction checkEnd() { 
-
-  // }; 
-  
   const source$ = merge(tick$)
-  .pipe(
-    scan((acc: State) => {
-      if (!acc.currentTetromino) return acc;
+    .pipe(scan((s: State) => ({ gameEnd: true }), initialState))
+    .subscribe((s: State) => {
+      render(s);
 
-      const newY = acc.currentTetromino.posY + 1;
-
-      // Check if the Tetromino can move down
-      if (canMove(acc.currentTetromino.shape, acc.board, acc.currentTetromino.posX, newY)) {
-        // Move the Tetromino down
-        return {
-          ...acc,
-          currentTetromino: {
-            ...acc.currentTetromino,
-            posY: newY,
-          },
-        };
+      if (s.gameEnd) {
+        show(gameover);
       } else {
-        // Lock the Tetromino in place and spawn a new one
-        const newBoard = [...acc.board];
-        for (let y = 0; y < acc.currentTetromino.shape.length; y++) {
-          for (let x = 0; x < acc.currentTetromino.shape[y].length; x++) {
-            if (acc.currentTetromino.shape[y][x]) {
-              newBoard[y + acc.currentTetromino.posY][x + acc.currentTetromino.posX] = 1;
-            }
-          }
-        }
-        return {
-          ...acc,
-          board: newBoard,
-          currentTetromino: {
-            shape: TETROMINOES[Math.floor(Math.random() * TETROMINOES.length)],
-            posX: Math.floor(BOARD_WIDTH / 2) - 1,
-            posY: 0,
-          },
-        };
+        hide(gameover);
       }
-    }, initialState)
-  )
-  .subscribe((s: State) => {
-    render(s);
-
-    if (s.gameEnd) {
-      show(gameover);
-    } else {
-      hide(gameover);
-    }
-  });
+    });
 }
 
 // The following simply runs your main function on window load.  Make sure to leave it in place.
